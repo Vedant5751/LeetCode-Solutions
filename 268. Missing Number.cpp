4002c318695371185257c0b1b3465148d09b47d8
Vedant Patil <vedant1346@gmail.com>
2024-10-08 15:28:01 +0530

    268. Missing Number Optimal

--- 268. Missing Number.cpp
+++ 268. Missing Number.cpp
@@ -3,16 +3,11 @@ class Solution
 public:
     int missingNumber(vector<int> &nums)
     {
-        sort(nums.begin(), nums.end());
-        int miss = nums.size();
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (i != nums[i])
-            {
-                miss = i;
-                break;
-            }
-        }
-        return miss;
+        int n = nums.size();
+        int sum = n * (n + 1) / 2;
+        int sum2 = 0;
+        for (int i = 0; i < n; i++)
+            sum2 += nums[i];
+        return (sum - sum2);
     }
 };
