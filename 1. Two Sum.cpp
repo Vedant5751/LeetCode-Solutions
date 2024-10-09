e733019e76b08fb833a8afe9fb34b3f3e315232e
Vedant Patil <vedant1346@gmail.com>
2024-10-09 14:40:05 +0530

    1. Two Sum

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int num = nums[i];
+            int required = target - num;
+            if (mpp.find(required) != mpp.end())
+            {
+                return {mpp[required], i};
+            }
+            mpp[num] = i;
+        }
+        return {};
+    }
+};
