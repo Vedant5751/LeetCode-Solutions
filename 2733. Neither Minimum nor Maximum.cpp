f8e835aab14f1f34bf4162a9a68531b377106fdf
Vedant Patil <vedant1346@gmail.com>
2024-10-24 22:52:48 +0530

    2733. Neither Minimum nor Maximum Better

--- 2733. Neither Minimum nor Maximum.cpp
+++ 2733. Neither Minimum nor Maximum.cpp
@@ -1,17 +1,13 @@
-class Solution
-{
+class Solution {
 public:
-    int findNonMinOrMax(vector<int> &nums)
-    {
-        if (nums.size() < 3)
-            return -1;
+    int findNonMinOrMax(vector<int>& nums) {
+        if(nums.size()<3) return -1;
         set<int> st;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            st.insert(nums[i]);
-        }
+        st.insert(nums[0]);
+        st.insert(nums[1]);
+        st.insert(nums[2]);
         set<int>::iterator it = st.begin();
         it++;
-        return *it;
+        return *it;        
     }
 };
