a7765b62317f472e75e823365ff308768ecd42ad
Vedant Patil <vedant1346@gmail.com>
2024-10-25 21:41:01 +0530

    169. Majority Element Optimal

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -1,5 +1,41 @@
-class Solution {
+class Solution
+{
 public:
+    int majorityElement(vector<int> &nums)
+    {
+        // Moore's Voting Algorithm
+        int cnt = 0;
+        int el;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (cnt == 0)
+            {
+                cnt = 1;
+                el = nums[i];
+            }
+            else if (el == nums[i])
+            {
+                cnt++;
+            }
+            else
+                cnt--;
+        }
+        int cnt1 = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == el)
+            {
+                cnt1++;
+            }
+        }
+        if (cnt1 > nums.size() / 2)
+            return el;
+        else
+            return -1;
+    }
+};
+
+/*
     int majorityElement(vector<int>& nums) {
         unordered_map<int, int> mpp;
         for(int i=0; i<nums.size(); i++){
@@ -11,4 +47,4 @@ public:
         }
         return -1;
     }
-};
+*/
