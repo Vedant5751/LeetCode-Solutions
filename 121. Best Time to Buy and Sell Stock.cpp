369734f5ed11314c59c7197730419975ad523b8d
Vedant Patil <vedant1346@gmail.com>
2024-10-26 23:24:38 +0530

    121. Best Time to Buy and Sell Stock

---
+++ 121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int mini = prices[0], profit = 0;
+        int cost;
+        for (int i = 0; i < prices.size(); i++)
+        {
+            cost = prices[i] - mini;
+            profit = max(cost, profit);
+            mini = min(mini, prices[i]);
+        }
+        return profit;
+    }
+};
